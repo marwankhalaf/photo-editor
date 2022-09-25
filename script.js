5834c45a298ec7eab2205cdd422bc7227f30e109
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2022-09-25 16:46:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let hueRotate = document.getElementById("hue-rotate");
+let download = document.getElementById("download");
+let reset = document.querySelector("span");
+let upload = document.getElementById("upload");
+let img = document.getElementById("img");
+let imgBox = document.querySelector(".img-box");
+let filters = document.querySelectorAll("ul li input")
+
+function resetValue(){
+    img.style.filter = "none";
+    saturate.value = "100";
+    contrast.value = "100";
+    brightness.value = "100";
+    sepia.value = "0";
+    grayscale.value = "0";
+    blur.value = "0";
+    hueRotate.value = "0";
+}
+
+window.onload = function (){
+    download.style.display = "none";
+    reset.style.display = "none";
+    imgBox.style.display = "none";
+}
+
+upload.onchange = function(){
+    
+    download.style.display = "block";
+    reset.style.display = "block";
+    imgBox.style.display = "block";
+
+    let file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    file.onload = function(){
+        img.src = file.result;
+    }
+    
+}
+
+filters.forEach( filter => {
+filter.addEventListener("input" , function(){
+    img.style.filter = `
+    saturate(${saturate.value}%)
+    contrast(${contrast.value}%)
+    brightness(${brightness.value}%)
+    sepia(${sepia.value}%)
+    grayscale(${grayscale.value}%)
+    blur(${blur.value}px)
+    hue-rotate(${hueRotate.value}deg)  
+    `
+})
+})
+
+
+
